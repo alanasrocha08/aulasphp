d08943652893e554fb99357430803750f879a7a6
alanasrocha08 <alanasr02@gmail.com>
2025-02-04 11:17:27 -0300

    Adiciona propriedades/atributos ao objeto

--- 04-objetos-genericos.php
+++ 04-objetos-genericos.php
@@ -10,11 +10,31 @@
     <hr>
 <?php
 /*Criando uma istância de uma classe genérica chamada stClass (Stander Class)*/ 
-$usuario = new stdClass();
+$usuario = new stdClass(); //instância ou objeto
+
+//Adicionando propriedades/atributos ao objeto
+$usuario -> nome = "Chapolin Colorado";
+$usuario ->idade = 25;
+$usuario -> email = "chapolin@gmail,com";
+$usuario ->telefones = ["11-2135-03000", "11-98745-7844"];
+
+//Pode ser usado {} ao redor do objeto/propriedade na interpolação
+echo $usuario ->nome;
+echo "<p><mark>{$usuario->nome}</mark><p;>"//interpolação
+
 ?>    
 
     <h2>Analisando a estrutura do objeto</h2>
     <pre><?=var_dump($usuario)?></pre>
+    <hr>
+
+    <h2>Saída de dados</h2>
+    <p>Nome: <?=$usuario-> nome?></p>
+    <p>Idade: <?=$usuario-> idade?>anos</p>
+    <p>Telefone: <?=$usuario-> telefones[1]?></p>
+
+    <hr>
+
     
     
 </body>

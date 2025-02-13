47c6c596c4762497c7b23a4b3d858e2176104b55
alanasrocha08 <alanasr02@gmail.com>
2025-02-13 11:19:49 -0300

    Adiciona incluide, requerie e recursos

---
+++ recusos.php
@@ -0,0 +1,8 @@
+<?php
+define("ESCOLA", "Senac");
+$curso = "Téc. Informática para internet";
+$tecnologias = ["HTML", "CSS", "JS"];
+
+function verificar (int $valorDaIdade):string{
+    return $valorDaIdDE >= 18 ? "maior" : "menor";
+}

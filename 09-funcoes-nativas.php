633c9fae1aaec42b6a41270f8781c41b1ac0d1a1
alanasrocha08 <alanasr02@gmail.com>
2025-02-13 09:46:09 -0300

    Aduiciona filter de validação de email

--- 09-funcoes-nativas.php
+++ 09-funcoes-nativas.php
@@ -107,6 +107,17 @@ $valorQualquer = 1259.75;
 
         <h2>Filtros</h2>
         <p>Recursos/Funções/Constantes de análise e limpeza de dados aplicados através das funções <code>filter_var()</code> e <code>filter_input()</code></p>
+
+        <h3>Validação</h3>
+<?php
+//Exemplo de email estruturado erroneamente
+$emailErrado = "alana.com.br";
+$emailCorreto = "alanasr02@gmail.com"
+?>
+        <p><?=var_dump(filter_var($emailErrado, FILTER_VALIDATE_EMAIL))?></p>
+        <p><?=var_dump(filter_var($emailCorreto, FILTER_VALIDATE_EMAIL))?></p>
+
+        <h3>Sanitização</h3>
     </div>
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>

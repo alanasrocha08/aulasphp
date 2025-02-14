10c7f9a8e1c697cf0a5818d13b3b4cd58b9b88a3
alanasrocha08 <alanasr02@gmail.com>
2025-02-14 11:08:11 -0300

    Exercício com desafio

--- 11-site/includes/cabecalho.php
+++ 11-site/includes/cabecalho.php
@@ -1,9 +1,31 @@
+<?php
+/*Captursmos o nome do arquivo atualmente aberto */
+$pagina = basename($_SERVER["PHP_SELF"]);
+
+//Analisamos o valor de $pagina para determinar um título personalizado
+switch ($pagina){
+    case 'index.php':
+        $titulo = "Página Inicial";
+        break;
+    case 'cursos.php':
+        $titulo = "Treinamentos";
+        break;
+    case 'duvidas.php':
+        $titulo = "Dúvidas";
+        break;
+    default:
+        $titulo = "Contato";
+        break;
+    
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site com PHP</title>
+    <title> <?=$titulo?> Site com PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
@@ -11,7 +33,7 @@
     <div class="container">
         <header>
             <h1>Site com PHP</h1>
-            <nav>
+            <nav class="text-center">
                 <a href="index.php">Home</a>
                 <a href="cursos.php">Treinamentos</a>
                 <a href="duvidas.php">Dúvidas</a>

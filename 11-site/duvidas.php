c11e02b173eb16ef76514779e23db0a3dc876d07
alanasrocha08 <alanasr02@gmail.com>
2025-02-14 08:52:45 -0300

    Adiciona cabeçalho com include

--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dúvidas - Site com PHP</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <header>
-            <h1>Site com PHP</h1>
-            <nav>
-                <a href="index.php">Home</a>
-                <a href="cursos.php">Cursos</a>
-                <a href="duvidas.php">Dúvidas</a>
-            </nav>
-        </header>
-
-        <main>
+<?php include "cabecalho.php" ?>
             <h2>Página de dúvidas</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, quasi nihil voluptates esse labore quod unde eveniet similique repudiandae corporis, eligendi porro molestias dolore corrupti nobis, saepe laborum. Nam, debitis.</p>
             <h3>Principais dúvidas</h3>

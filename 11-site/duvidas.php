05cb8dff15f19ee893d9434b41fe1c9fc3f3306d
alanasrocha08 <alanasr02@gmail.com>
2025-02-14 09:00:34 -0300

    Adiciona include rodapé

--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -1,4 +1,4 @@
-<?php include "cabecalho.php" ?>
+<?php include "cabecalho.php"; ?>
             <h2>Página de dúvidas</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, quasi nihil voluptates esse labore quod unde eveniet similique repudiandae corporis, eligendi porro molestias dolore corrupti nobis, saepe laborum. Nam, debitis.</p>
             <h3>Principais dúvidas</h3>
@@ -11,11 +11,4 @@
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum repellat, cumque accusamus nam vitae consequatur aliquam, reiciendis sed quo ut iste obcaecati nihil explicabo distinctio! Voluptatem veniam libero dolorum architecto?</p>
             </details>
         </main>
-        <footer>
-            <p>Site criado para o curso Téc. Informática para Internet</p>
-        </footer>
-    </div>
-    
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include "rodape.php";?>
--- 11-site/index.php
+++ 11-site/index.php
@@ -1,13 +1,6 @@
-<?php include "cabecalho.php" ?>
+<?php include "cabecalho.php"; ?>
             <h2>Bem-vindo ao site Exemplo</h2>
             <p>Esta é a primeira página do site.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere amet exercitationem magnam voluptate consequatur necessitatibus sint, velit numquam dolorem est facilis quaerat repellat aperiam, possimus repellendus. Sint eius vero harum!</p>
         </main>
-        <footer>
-            <p>Site criado para o curso Téc. Informática para Internet</p>
-        </footer>
-    </div>
-    
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include "rodape.php"; ?>
